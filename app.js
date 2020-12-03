36e1c014fc7c67c475f2b96467066b0689cf8234
LF-MI-Team <tom_bombchild@hotmail.com>
2020-12-03 21:58:56 +0000

    turned the docx module into a sub directory to cover the changes I made to it

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const passport = require("passport");
 
 
 const ssrs = require('mssql-ssrs');
-const DocxMerger = require('docx-merger');
+const DocxMerger = require('./util/docx-merger/index.js');
+// const DocxMerger = require('docx-merger');
 // var builder = require('docx-builder');
 const docx = require("docx");
 
