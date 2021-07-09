4150505124cd1448ff8e8153df87ef3ad28c9fcf
Thomas Cassady <thomas.cassady@littlefish.co.uk>
2021-07-09 08:51:04 +0100

    added clause that only active subscriptions will be added to the scheduler.

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -35,67 +35,69 @@ exports.updateScheduler = async() => {
 
             report.subscriptions.forEach((subscription) => {
 
-                let info = subscription
+                if(subscription.active === 1){
 
-                let y = subscription.start_date.substring(0, 4)
-                let m = parseInt(subscription.start_date.substring(5, 7))-1
-                let d = subscription.start_date.substring(8, 10)
-                let h = subscription.time.substring(0, 2)
-                let M =subscription.time.substring(3, 5)
-                let start_date = new Date(y, m, d, h, M)
-                
+                    let y = subscription.start_date.substring(0, 4)
+                    let m = parseInt(subscription.start_date.substring(5, 7))-1
+                    let d = subscription.start_date.substring(8, 10)
+                    let h = subscription.time.substring(0, 2)
+                    let M =subscription.time.substring(3, 5)
+                    let start_date = new Date(y, m, d, h, M)
+                    
+    
+                    let date = start_date.getDate();
+                    let day = start_date.getDay();
+                    // let hour = start_date.getHours();
+                    // let minute = start_date.getMinutes();
+    
+                    let options;
+                    
+                    switch(subscription.frequency.name){
+                        case "Daily":
+                            options = {
+                                second: "",
+                                minute: M,
+                                hour: h,
+                                day_of_month: '*', 
+                                month: '*',
+                                day_of_week: '*',
+                                report: report,
+                                subscription: subscription 
+                            }
+                            exports.addSchedule(options)
+                            break;
+                        case "Weekly":
+                            options = {
+                                second: "",
+                                minute: M,
+                                hour: h,
+                                day_of_month: '*', 
+                                month: '*',
+                                day_of_week: day,
+                                report: report,
+                                subscription: subscription 
+                            }                        
+                            exports.addSchedule(options)                        
+                            break;
+                        case "Monthly":
+                            options = {
+                                second: "",
+                                minute: M,
+                                hour: h,
+                                day_of_month: '*', 
+                                month: m,
+                                day_of_week: '*',
+                                report: report,
+                                subscription: subscription 
+                            }                            
+                            exports.addSchedule(options)                         
+                            break;                                                    
+                    }
+    
+                    // console.log("test")
+    
 
-                let date = start_date.getDate();
-                let day = start_date.getDay();
-                // let hour = start_date.getHours();
-                // let minute = start_date.getMinutes();
-
-                let options;
-                
-                switch(subscription.frequency.name){
-                    case "Daily":
-                        options = {
-                            second: "",
-                            minute: M,
-                            hour: h,
-                            day_of_month: '*', 
-                            month: '*',
-                            day_of_week: '*',
-                            report: report,
-                            subscription: subscription 
-                        }
-                        exports.addSchedule(options)
-                        break;
-                    case "Weekly":
-                        options = {
-                            second: "",
-                            minute: M,
-                            hour: h,
-                            day_of_month: '*', 
-                            month: '*',
-                            day_of_week: day,
-                            report: report,
-                            subscription: subscription 
-                        }                        
-                        exports.addSchedule(options)                        
-                        break;
-                    case "Monthly":
-                        options = {
-                            second: "",
-                            minute: M,
-                            hour: h,
-                            day_of_month: '*', 
-                            month: m,
-                            day_of_week: '*',
-                            report: report,
-                            subscription: subscription 
-                        }                            
-                        exports.addSchedule(options)                         
-                        break;                                                    
                 }
-
-                // console.log("test")
-
             })
 
         }
