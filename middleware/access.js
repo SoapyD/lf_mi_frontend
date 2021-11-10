0c38c4dc2cc90c15dfd23c5995247828109ecaf2
Thomas Cassady <thomas.cassady@littlefish.co.uk>
2021-11-10 09:03:18 +0000

    small edit to middleware

--- middleware/access.js
+++ middleware/access.js
@@ -19,3 +19,24 @@ exports.isAdmin = function(req, res, next){
     res.redirect("/")
 }
 
+/*
+exports.isClientDataAdmin = function(req, res, next){
+    if(req.user){
+        if (req.user.role === "App_Admin" || req.user.role === "ClientData_Admin") {
+            return next();
+        }
+    }
+    req.flash("error", "You are denied access as you do not have an admin role. Please contact your system admin to gain access");
+    res.redirect("/")
+}
+
+exports.isUserDataAdmin = function(req, res, next){
+    if(req.user){
+        if (req.user.role === "App_Admin" || req.user.role === "UserData_Admin") {
+            return next();
+        }
+    }
+    req.flash("error", "You are denied access as you do not have an admin role. Please contact your system admin to gain access");
+    res.redirect("/")
+}
+*/
